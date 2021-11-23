e2c97a7066bbaf8777d6b0a553d81e54a9a0e3bf
alfranc <alexfranco12@gmail.com>
2021-11-23 09:07:03 -0600

    added a profile button

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -1,13 +1,28 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Modal } from "..";
+import { RiUserLine } from 'react-icons/ri'
 
 export const NavBar = () => {
+  const [showModal, setShowModal] = useState(false)
+
   return ( 
     <NavBarStyled>
-      <h1 className="title">PAINT</h1>
-      <Link to="/login" className="login">
-        login
+      <Link to="/">
+        <h1 className="title">PAINT</h1>
       </Link>
+      
+      <div className="profile-link" >
+        <div
+          className="link"
+          onClick={() => setShowModal(true)}>
+          <RiUserLine />
+        </div>
+      </div>
+      
+      <Modal showModal={showModal} setShowModal={setShowModal}/>
+
     </NavBarStyled>
    );
 };
@@ -18,19 +33,32 @@ const NavBarStyled = styled.div`
   display: grid;
   grid-template-columns: subgrid;
   grid-template-rows: auto;
-  & .title {
-    grid-column: 1 / span 7;
-    grid-row: 1;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
+  position: relative;
+  & a {
+    color: ${props => props.theme.colors.dark1};
+    & .title {
+      grid-column: 1 / span 7;
+      grid-row: 1;
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+    }
   }
-  & .login {
+  & .profile-link {
     grid-column: 8 / span 2;
     grid-row: 1;
     display: flex;
     justify-content: center;
     align-items: center;
+    & .link {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border: 1px solid black;
+      border-radius: 4px;
+      padding: .75rem 1.25rem;
+      background-color: ${props => props.theme.colors.secondary1};
+    }
   }
 
 
