4726ccbe3ceef06ecf72304eb20643dee006dd7e
alfranc <alexfranco12@gmail.com>
2021-11-24 13:28:33 -0600

    modified the useAxios hook slightly

--- src/utils/useAxios.js
+++ src/utils/useAxios.js
@@ -1,32 +1,53 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const { NODE_ENV, REACT_APP_BACKEND, REACT_APP_HEROKU_BACKEND } = process.env;
+const { REACT_APP_ENV, REACT_APP_BACKEND, REACT_APP_HEROKU_BACKEND } = process.env;
 axios.defaults.baseURL = (
-  NODE_ENV === "development" 
+  REACT_APP_ENV === "development" 
     ? REACT_APP_BACKEND : REACT_APP_HEROKU_BACKEND
 )
 
-const useAxios = ( params ) => {
+const useAxios = ( config ) => {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  const fetchData = async ( params ) => {
+  const fetchData = async ( config, source, isMounted ) => {
     setLoading(true)
+    
     try {
-      const res = await axios.request(params);
-      setData(res.data);
-      setError(null);
+      const res = await axios.request({
+        cancelToken: source.token,
+        ...config
+      });
+      if (isMounted) {
+        setData(res.data)
+        setError(null)
+      }
     } catch (err) {
-      setError(err);
+      if (isMounted) {
+        setError(err);
+        setData([])
+      }
     } finally {
-      setLoading(false);
+      isMounted && setLoading(false);
     }
   };
 
   useEffect(() => {
-    fetchData();
+    const source = axios.CancelToken.source();
+    let isMounted = true;
+
+    fetchData(config, source, isMounted);
+
+    const cleanUp = () => {
+      isMounted = false;
+      source.cancel();
+    }
+
+    return cleanUp;
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return { data, error, loading };
